ce06fd12b81a2e2182f5561a8c2c8c34bb459586
hageromar <hageromar328@gmail.com>
2025-02-10 02:14:25 +0200

    update statefull to less

--- lib/widget/future.dart
+++ lib/widget/future.dart
@@ -3,28 +3,56 @@ import 'package:news_applications/model/newsverti.dart';
 import 'package:news_applications/services/newsService.dart';
 import 'package:news_applications/widget/verti/listItem.dart';
 
-class FutureListNews extends StatefulWidget {
+// class FutureListNews extends StatefulWidget {
+//   const FutureListNews({super.key});
+
+//   @override
+//   State<FutureListNews> createState() => _FutureListNewsState();
+// }
+
+// class _FutureListNewsState extends State<FutureListNews> {
+//   var Future;
+//   @override
+//   void initState() {
+//     super.initState();
+//     Future = Newsservice().getnews();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return FutureBuilder<List<NewsVerticalModel>>(
+//         future: Future,
+//         builder: (context, snapshot) {
+//           if (snapshot.hasData) {
+//             return ListItemVerti(
+//               moodel: snapshot.data!,
+//             );
+//           } else if (snapshot.hasError) {
+//             return const SliverToBoxAdapter(
+//               child: Text('oops error'),
+//             );
+//           } else {
+//             return const SliverToBoxAdapter(
+//               child: Center(
+//                 child: CircularProgressIndicator(),
+//               ),
+//             );
+//           }
+//         });
+//   }
+// }
+class FutureListNews extends StatelessWidget {
   const FutureListNews({super.key});
 
   @override
-  State<FutureListNews> createState() => _FutureListNewsState();
-}
-
-class _FutureListNewsState extends State<FutureListNews> {
-  var Future;
-  @override
-  void initState() {
-    super.initState();
-    Future=Newsservice().getnews();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return FutureBuilder<List<NewsVerticalModel>>(
-        future: Future,
+         return FutureBuilder<List<NewsVerticalModel>>(
+        future: Newsservice().getnews(),
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return ListItemVerti(moodel: snapshot.data!);
+            return ListItemVerti(
+              moodel: snapshot.data!,
+            );
           } else if (snapshot.hasError) {
             return const SliverToBoxAdapter(
               child: Text('oops error'),
